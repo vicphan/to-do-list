59072a6798dd6cfad49ba7953885cb6c3d06564d
Vic Phan <vphan9@gmail.com>
2022-02-25 23:29:04 -0700

    add task form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import AddTask from "./components/AddTask"
 
 const App = () => {
   return (
     <div className="container">
       <Header />
+      <AddTask />
     </div>
   )
 }
---
+++ src/components/AddTask.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+const AddTask = () => {
+
+    const [text, setText] = useState("")
+    const [date, setDate] = useState(new Date())
+
+    return (
+        <div class="add-task-container">
+            <form className="add-task-form">
+                <div className='form-control'>
+                    <label>Task</label>
+                    <input type="text" placeholder="Task" value={text} />
+                </div>
+                <div className='form-control'>
+                    <label>Due Date</label>
+                    <input type="date" value={date} />
+                </div>
+                <input type="submit" value="Save" className="save-button"/>
+            </form>
+        </div>
+    )
+}
+
+export default AddTask
